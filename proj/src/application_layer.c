67bb237bce3f8b50eca476ce03955573c66e8261
Pedro <up202008467@edu.fe.up.pt>
2023-10-23 22:19:28 +0100

    Declaracao de variaveis em apliccationLayer

--- proj/src/application_layer.c
+++ proj/src/application_layer.c
@@ -5,5 +5,17 @@
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    LinkLayer linkLayer;
+
+    strcpy(linkLayer.serialPort, serialPort);
+
+    linkLayer.baudRate = baudRate;
+    linkLayer.nRetransmissions = nTries;
+    linkLayer.timeout = timeout;
+    
+    if(strcmp(role, "rx"))
+       linkLayer.role = LlRx; 
+    else if(strcmp(role, "tx"))
+        linkLayer.role = LlTx;
+
 }
