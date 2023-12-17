c429768fd56364fa51efc8176f6fdca9ee828fc7
Pedro Gomes <up202108825@up.pt>
2023-12-17 17:58:14 +0000

    structure

---
+++ proj2/src/download.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // usage
+    // parse 
+    // criar socket a
+    // autenticacao
+    // passive mode
+    // criar socket b
+    // request resource
+    // get resource
+    // close connection
+    return 0;
+}
